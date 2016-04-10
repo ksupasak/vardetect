9000cd13ab1035aeef044e104e7506d2447b1769
worawich <voravich1@hotmail.com>
2016-04-10 18:26:51 +0700

    Add sim whole genome data

--- NGS-VarDetect/NGS-VarDetect-Core/src/biotec/bsi/ngs/vardetect/core/util/SimulatorUtil_aon.java
+++ NGS-VarDetect/NGS-VarDetect-Core/src/biotec/bsi/ngs/vardetect/core/util/SimulatorUtil_aon.java
@@ -8,6 +8,8 @@ package biotec.bsi.ngs.vardetect.core.util;
 import biotec.bsi.ngs.vardetect.core.ChromosomeSequence;
 import biotec.bsi.ngs.vardetect.core.InputSequence;
 import biotec.bsi.ngs.vardetect.core.ReferenceSequence;
+import biotec.bsi.ngs.vardetect.core.ShortgunSequence;
+import java.util.Random;
 import java.util.Vector;
 
 /**
@@ -16,12 +18,55 @@ import java.util.Vector;
  */
 public class SimulatorUtil_aon {
     
-    public static InputSequence simulateWholeGene(ReferenceSequence ref, int num_read, int ln_read){
-        
+    public static InputSequence simulateWholeGene(ReferenceSequence ref, int num_read, int ln_read, int numberchrA,int numberchrB){
+        String namechrA = "chr"+numberchrA;
+        String namechrB = "chr"+numberchrB;
+        ChromosomeSequence chrA = null,chrB = null;
         InputSequence is = new InputSequence();
-        Vector<ChromosomeSequence> chrs = ref.getChromosomes();
+        Random rand = new Random();
         
-        System.out.println(chrs);
+        Vector<ChromosomeSequence> chrs = ref.getChromosomes();
+        System.out.println(chrs.size());
+       
+        for(int chrNum=0;chrNum<chrs.size();chrNum++){
+        
+            System.out.println("chr number" + chrNum);
+            ChromosomeSequence chr = chrs.elementAt(chrNum);
+            System.out.println("Chromosome name"+chr.getName());
+
+            if (chr.getName().equalsIgnoreCase(namechrA)){
+                chrA = chr;
+            }
+            else if (chr.getName().equalsIgnoreCase(namechrB)){
+                chrB = chr;
+            }   
+        }
+        
+        CharSequence iniTemplate = SequenceUtil.concatenateChromosome(chrA, chrB, ln_read-1, ln_read-1);
+        
+        String read;
+        for(int i = 0;i<num_read;i++){
+            int iniread = rand.nextInt(ln_read);
+            
+            read = iniTemplate.subSequence(iniread, iniread+ln_read).toString();
+            
+            System.out.println("Initial position: " + iniread);
+            System.out.println("Number of base from " + namechrA + " : " + ((ln_read-iniread)-1));
+            System.out.println("Number of base from " + namechrB + " : " + (iniread+1));
+            System.out.println(read);
+            
+            ShortgunSequence ss = new ShortgunSequence(read);
+            is.seqs.add(ss);         
+        }
+        
+        //while(chrs.elements()..hasMoreElements()){
+            
+            //ChromosomeSequence chr = chrs.elements().nextElement();
+            
+            //System.out.println(chr.getName());
+            
+        //}
+        /*System.out.println(chrs.elements().hasMoreElements());*/
         
         
         
