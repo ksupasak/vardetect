f6891d981e9c77194ebeb9b3457f9b79cb402857
worawich <voravich1@hotmail.com>
2016-04-11 15:16:33 +0700

    Test ExonIntron and random pick

--- NGS-CMD/src/biotec/bsi/ngs/vardetect/cmd/NGSCMD2.java
+++ NGS-CMD/src/biotec/bsi/ngs/vardetect/cmd/NGSCMD2.java
@@ -7,6 +7,7 @@ package biotec.bsi.ngs.vardetect.cmd;
 
 import biotec.bsi.ngs.vardetect.core.ExonIntron;
 import biotec.bsi.ngs.vardetect.core.InputSequence;
+import biotec.bsi.ngs.vardetect.core.ReferenceExonIntron;
 import biotec.bsi.ngs.vardetect.core.ReferenceSequence;
 import biotec.bsi.ngs.vardetect.core.util.SequenceUtil;
 import biotec.bsi.ngs.vardetect.core.util.SimulatorUtil_aon;
@@ -16,6 +17,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Random;
+import java.util.Vector;
 
 /**
  *
@@ -25,7 +28,12 @@ public class NGSCMD2 {
     
     public static void main(String args[]) throws FileNotFoundException, IOException{
         
-        ExonIntron ee = SequenceUtil.readExonIntron(args[3]);
+        ReferenceExonIntron ee = SequenceUtil.readExonIntron(args[3]);
+        Vector<ExonIntron> data = ee.getdata();
+        System.out.println("Vector size" + data.size());
+        System.out.print("Pick" + data.elementAt(500).getdirection());
+        int iniguess = Math.abs(data.elementAt(500).getdirection());
+        //for (int i == data.elementAt(500).getdirection())
         
         //ReferenceSequence ref = SequenceUtil.readReferenceSequence(args[1]);
         //InputSequence is = new InputSequence();  
