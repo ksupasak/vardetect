04230957654d24dd5c823cb12164a72739c93514
worawich <voravich1@hotmail.com>
2016-09-05 17:03:05 +0700

    Cancel generate compliment Reference Use create compliment of Read and align on main Reference instead
    (	Alignment status:	All align		)
    (	Report status	:	in process		)	
    (	Strand check	:	Done			)	
    (	Reconstruct		:	Done			)

--- NGS-CMD/src/biotec/bsi/ngs/vardetect/cmd/NGSCMD7.java
+++ NGS-CMD/src/biotec/bsi/ngs/vardetect/cmd/NGSCMD7.java
@@ -7,50 +7,53 @@ package biotec.bsi.ngs.vardetect.cmd;
 
 import biotec.bsi.ngs.vardetect.alignment.AlignerFactory;
 import biotec.bsi.ngs.vardetect.core.Aligner;
-import biotec.bsi.ngs.vardetect.core.AlignmentResult;
 import biotec.bsi.ngs.vardetect.core.AlignmentResultRead;
-import biotec.bsi.ngs.vardetect.core.ChromosomeSequence;
 import biotec.bsi.ngs.vardetect.core.ClusterGroup;
-import biotec.bsi.ngs.vardetect.core.EncodedSequence;
 import biotec.bsi.ngs.vardetect.core.InputSequence;
 import biotec.bsi.ngs.vardetect.core.ReferenceSequence;
 import biotec.bsi.ngs.vardetect.core.ShortgunSequence;
 import biotec.bsi.ngs.vardetect.core.util.Clustering;
 import biotec.bsi.ngs.vardetect.core.util.SequenceUtil;
-import static biotec.bsi.ngs.vardetect.core.util.SequenceUtil.encodeSerialChromosomeSequenceV3;
-import biotec.bsi.ngs.vardetect.core.util.SimulatorUtil;
 import biotec.bsi.ngs.vardetect.core.util.SimulatorUtil_WholeGene;
 import biotec.bsi.ngs.vardetect.core.util.VisualizeResult;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  *
  * @author worawich
  */
-
-public class NGSCMD4 {
-    
-     public static void main(String[] args) throws IOException {
-        // TODO code application logic here
-        
-//       ReferenceSequence ref = SequenceUtil.readAndIndexReferenceSequence("/Users/soup/Desktop/hg19/hg19.fa");
-        Map<String,ArrayList<Map>> aon = new HashMap();
-        System.out.println(aon==null);
-        System.out.println(aon!=null);
+public class NGSCMD7 {
+    public static void main(String[] args) throws IOException {
         
         System.out.println("Get reference sequence");
         ReferenceSequence ref = SequenceUtil.getReferenceSequence(args[0]); //runFile hg19.fa
         
         //ChromosomeSequence c = ref.getChromosomeSequenceByName("chr21");
-        System.out.println("Simulate Data");
+        System.out.println("Read Data");
         //InputSequence input =  SimulatorUtil_WholeGene.simulateWholeGene(ref, 5, 100, "20", "21");
-        InputSequence input =  SimulatorUtil_WholeGene.simulateComplexWholeGeneRandom(ref,1, 100, 5);
+//        ShortgunSequence tempSS = SequenceUtil.readInputFile(args[1]);
+       
+        String sequence = "ATGGCACATGCAGCGCAAGTAGGTCTACAAGACGCTACTTCCCCTATCATAGAAGAGCTTATCACCTTTCATGAT" +
+"CACGCCCTCATAATCATTTTCCTTATCTGCTTCCTAGTCCTGTATGCCCTTTTCCTAACACTCACAACAAAACTA" +
+"ACTAATACTAACATCTCAGACGCTCAGGAAATAGAAACCGTCTGAACTATCCTGCCCGCCATCATCCTAGTCCTC" +
+"ATCGCCCTCCCATCCCTACGCATCCTTTACATAACAGACGAGGTCAACGATCCCTCCCTTACCATCAAATCAATT" +
+"GGCCACCAATGGTACTGAACCTACGAGTACACCGACTACGGCGGACTAATCTTCAACTCCTACATACTTCCCCCA" +
+"TTATTCCTAGAACCAGGCGACCTGCGACTCCTTGACGTTGACAATCGAGTAGTACTCCCGATTGAAGCCCCCATT" +
+"CGTATAATAATTACATCACAAGACGTCTTGCACTCATGAGCTGTCCCCACATTAGGCTTAAAAACAGATGCAATT" +
+"CCCGGACGTCTAAACCAAACCACTTTCACCGCTACACGACCGGGGGTATACTACGGTCAATGCTCTGAAATCTGT" +
+"GGAGCAAACCACAGTTTCATGCCCATCGTCCTAGAATTAATTCCCCTAAAAATCTTTGAAATAGGGCCCGTATTT" +
+"ACCCTATAG"; // got this Sequence from http://molevol.lysine.umiacs.umd.edu/resources/fileformats on the fasta unAlign Example file.
+        ShortgunSequence inSS = new ShortgunSequence(sequence);
+        inSS.addReadName("Un_Align_EX");
+        InputSequence input = new InputSequence();
+//        input.addRead(inSS);
+        //input = SequenceUtil.readSampleFile(args[1]);
+        String fixPath = "/Users/worawich/VMdev/3661/output.fa";
+        input = SequenceUtil.readSampleFileV2(fixPath,100000,200000);
+//        InputSequence input =  SimulatorUtil_WholeGene.simulateComplexWholeGeneRandom(ref,5, 100, 5);
         
-        
+        System.out.println("********** Do Alignment *********");
         Aligner aligner = AlignerFactory.getAligner();          // Will link to BinaryAligner
           
         AlignmentResultRead align = aligner.align(ref, input);  // function align is located in binary aligner
@@ -68,9 +71,11 @@ public class NGSCMD4 {
         //System.out.println("Size of Result: " + align.getAlignmentCount().size());
         
 //        VisualizeResult.visualizeAlignmentCountMatchCutPlusColor(align,100);
-        
+        System.out.println("********** Do Write Result SortedResult *********");
         align.writeSortedResultToPath(ref.getPath(), "txt");
+        System.out.println("********** Do Write Result UnSortedResult *********");
         align.writeUnSortedResultToPath(ref.getPath(), "txt");
+        System.out.println("********** Do Write Result CutResult *********");
         align.writeSortedCutResultToPath(ref.getPath(), "txt", 5);
         /* Old Grouping algorithm
         align.createAllClusterCode();
@@ -86,20 +91,24 @@ public class NGSCMD4 {
         align.createGroupingResult();
         System.out.println(" ****** Check cluster result: " + align.getclusterResult().size());
         */
-        
+        System.out.println("********** Do Calculate Euclidient Distance *********");
         align.calculateEuclidientdistance(); // Must have this order before clustering
 //        VisualizeResult.visualizeDistanceTable(align);
+        System.out.println("********** Do Write Result DistanceTable *********");
         align.writeDistanceTableToPath(ref.getPath(), "txt");
         
-        
+        System.out.println("********** Do Clustering Group *********");
         ArrayList<ClusterGroup> groupResult = Clustering.clusteringGroup(align, 100);
         align.addGroupReult(groupResult);
+        System.out.println("********** Do Write Group Result *********");
         align.writeClusterGroupToPath(ref.getPath(), "txt");
         
         System.out.println(" check number of group : " + groupResult.size());
         VisualizeResult.visualizeClusterGoup(groupResult);
         
+        System.out.println("********** Do Reconstruct Sequence *********");
         align.enableReconstruct();
+        System.out.println("********** Do Write Pattern Report *********");
         align.writePatternReport(ref.getPath(), "txt");
         
         // Create save result path by just plugin AlignmentResult
@@ -133,5 +142,5 @@ public class NGSCMD4 {
         }*/
     
     }
-
+    
 }
