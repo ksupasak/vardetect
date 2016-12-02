d12e30293963f7cbb960923e237a7e39bf1d8fdc
worawich <voravich1@hotmail.com>
2016-12-02 16:56:00 +0700

    Cancel generate compliment Reference Use create compliment of Read and align on main Reference instead
    (	Alignment status		:	All align		)
    (	Report status			:	in process		)	
    (	Strand check			:	Done			)	
    (	Reconstruct				:	Done			)
    (	Multi-Thread			:	Done			)
    (	Optimize mem(Align) 	:	Done			)
    (	Optimize mem(Cluster)	:	in process		)
    (	Second approach(Cluster):	in process			)
    (	Second approach(color)	:	Done			)
    (	New structure V3		: 	Done			)
    (	Cluster new structure 	:	in Progress		)
    (	Color detect V3		:	Done			)

--- NGS-CMD/src/biotec/bsi/ngs/vardetect/cmd/TestPostProcess.java
+++ NGS-CMD/src/biotec/bsi/ngs/vardetect/cmd/TestPostProcess.java
@@ -23,11 +23,11 @@ public class TestPostProcess {
        public static void main(String[] args) throws IOException {
         // TODO code application logic here
         
-        String filename = "hg19test_New_structure_with_iniIndex_check";
+        String filename = "hg193661_alignmentResult_part";       
         String path = "/Users/worawich/VMdev/dataScieneToolBox/projects/NGS/";
-        String saveFileName = "3661_Mul_LinuxSortFormat";
-        String filename3661 = "hg19_3661_mul_alignmentResult_th2_part";
-        String saveFileName3661 = "hg19_3661_mul_alignmentResult_th2_forLinuxSortV2";
+        String saveFileName = "hg19_3661_alignmentResult_forLinuxSort";
+        String filename3661 = "hg19_3661_mul_alignmentResult_th5_part";
+        String saveFileName3661 = "hg19_3661_mul_alignmentResult_th5_forLinuxSort";
     
         for(int i=1;i<=26;i++){
             
@@ -35,7 +35,7 @@ public class TestPostProcess {
             System.out.println("Begin create color array");
             Clustering.createColorArray(readAlign, 100, 18);        
             System.out.println("Done create color array");
-            readAlign.writeSortedCutColorResultToPathInFormatForLinuxSort(path, saveFileName3661, "txt");
+            readAlign.writeSortedCutColorResultToPathInFormatForLinuxSort(path, saveFileName3661, "txt","gy");
             readAlign = null;
             System.gc();
        }
