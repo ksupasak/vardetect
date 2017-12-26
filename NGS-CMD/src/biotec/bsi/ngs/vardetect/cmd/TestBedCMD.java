ae80ef51b617a7bbaef4d8216eb075c223513111
worawich <worawich@10.226.76.76>
2017-12-26 11:57:41 +0700

    - add hard code for extract read name (not important just a quick work)

--- NGS-CMD/src/biotec/bsi/ngs/vardetect/cmd/TestBedCMD.java
+++ NGS-CMD/src/biotec/bsi/ngs/vardetect/cmd/TestBedCMD.java
@@ -23,7 +23,9 @@ import biotec.bsi.ngs.vardetect.core.util.SimulatorUtil;
 import biotec.bsi.ngs.vardetect.core.util.SimulatorUtil_WholeGene;
 import biotec.bsi.ngs.vardetect.core.util.VisualizeResult;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -45,6 +47,8 @@ public class TestBedCMD {
         String sampleFile = "/Volumes/PromisePegasus/worawich/Download_dataset/Micro_RNA/NGS_result_300517/OP4_S4_L001_R2_001_cutadapt_filter.fa";
         String nonVariantFile = "/Volumes/PromisePegasus/worawich/Download_dataset/cancer/unmaped_cancer/TCGA_75_5147_virus_alignResult_Sort.txt";
         String coverageFile = "/Volumes/PromisePegasus/worawich/Download_dataset/cancer/unmaped_cancer/TCGA_75_5147_virus_alignResult_Sort_nonVariantCoverage.txt";
+        
+        
 //          int startPoint = 0;
 //          int length = 15;
 //          SequenceUtil.truncateFastaFIles(fastaFile, startPoint, length);
@@ -57,13 +61,53 @@ public class TestBedCMD {
         
 //        FastaUtil.separateContigToFile(sampleFile);
 
-        File aFile = new File("/Volumes/PromisePegasus/worawich/Download_dataset/Test_ERR718259_genRef/testline.txt");
-        System.out.println(aFile.getAbsoluteFile());
-        System.out.println(aFile.getAbsolutePath());
+        /**
+         * Code below use to extract read name from variant report (this file is a file that we use grep command to cut out only interest group along with the first pattern)
+         * and write only read name to new file
+         */
+        String file = "/Users/worawich/Google Drive/WORK/TB_Urgent/Result_RD239and105/sum_FoundRD239.txt";
+        ArrayList<String> readList = new ArrayList();
+        RandomAccessFile rb = new RandomAccessFile(file,"rw");
+        String line = "";
+        while ((line = rb.readLine()) != null) {
+            
+            if(line.charAt(0)=='G'){
+                
+            }else if(line.charAt(0) == '-'){
+            }else{
+                String[] compLine = line.split(",");
+                String[] readNameFull = compLine[9].split("\\.");
+                String read = readNameFull[0];
+                
+                if(!readList.contains(read)){
+                    readList.add(read);
+                }
+            }
+        }
         
-        System.out.println(aFile.getName());
-        System.out.println(aFile.getPath());
-        System.out.println(aFile.getParent());
+        FileWriter writer;
+        /**
+         * Check File existing
+         */
+        
+        File f = new File(file.split("\\.")[0]+"_readName.txt"); //File object
+        writer = new FileWriter(f);
+        for(int i = 0;i<readList.size();i++){
+            writer.write(readList.get(i));
+            writer.write("\n");
+        }
+        
+        writer.flush();
+        writer.close();
+        /*******************************************************/
+        
+//        File aFile = new File("/Volumes/PromisePegasus/worawich/Download_dataset/Test_ERR718259_genRef/testline.txt");
+//        System.out.println(aFile.getAbsoluteFile());
+//        System.out.println(aFile.getAbsolutePath());
+//        
+//        System.out.println(aFile.getName());
+//        System.out.println(aFile.getPath());
+//        System.out.println(aFile.getParent());
         
         
 
