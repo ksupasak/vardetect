e341b4a25c2921a04203e81cbb2f5d59eb7df53f
worawich <worawich@10.226.76.92>
2018-03-09 15:26:14 +0700

    - change variable and function version for testing

--- NGS-CMD/src/biotec/bsi/ngs/vardetect/cmd/TestUnIntersectRead.java
+++ NGS-CMD/src/biotec/bsi/ngs/vardetect/cmd/TestUnIntersectRead.java
@@ -21,9 +21,9 @@ public class TestUnIntersectRead {
         // implement md5 check for sequence similarity (with sequence md5 check it can get rid of read that has the same sequence base but diferent in read name also)
         // fq1 should be the smallest file that contain read that we want to filter out
         
-//        String fq1 = "/Users/worawich/Download_dataset/TB/SampleForTestScript/debug/ERR718193_C.fq";
-//        String fq2 = "/Users/worawich/Download_dataset/TB/SampleForTestScript/debug/ERR718193_map_preA.fq.gz";
-//        String saveFileName = "/Users/worawich/Download_dataset/TB/SampleForTestScript/debug/testmd5unintersect.fq";
+//        String fq1 = "/Users/worawich/Download_dataset/HiSeq_X_Ten_TruSeq_Nano_4_replicates_of_NA12878_-8998991/BWA_Whole_Genome_Sequencing_NA12878_L1-10532644/NA12878_L1-ds_d51e1a3660b647f09677f58400c86764/NA12878-L1_S1_C.fq.gz";
+//        String fq2 = "/Users/worawich/Download_dataset/HiSeq_X_Ten_TruSeq_Nano_4_replicates_of_NA12878_-8998991/BWA_Whole_Genome_Sequencing_NA12878_L1-10532644/NA12878_L1-ds_d51e1a3660b647f09677f58400c86764/NA12878-L1_S1_map_preA.fq.gz";
+//        String saveFileName = "/Users/worawich/Download_dataset/HiSeq_X_Ten_TruSeq_Nano_4_replicates_of_NA12878_-8998991/BWA_Whole_Genome_Sequencing_NA12878_L1-10532644/NA12878_L1-ds_d51e1a3660b647f09677f58400c86764/testmd5unintersect.fq.gz";
         String fq1 = args[0];
         String fq2 = args[1];
         String saveFileName = args[2];
--- NGS-CMD/src/biotec/bsi/ngs/vardetect/cmd/testModifyBam.java
+++ NGS-CMD/src/biotec/bsi/ngs/vardetect/cmd/testModifyBam.java
@@ -15,8 +15,10 @@ import java.security.NoSuchAlgorithmException;
  */
 public class testModifyBam {
     public static void main(String[] args) throws NoSuchAlgorithmException, IOException{
-        String mainBamFile = "/Users/worawich/Download_dataset/TB/SampleForTestScript/debug/ERR718193.bam";
-        String targetBamFile = "/Users/worawich/Download_dataset/TB/SampleForTestScript/debug/ERR718193_C_map.bam";
+//        String mainBamFile = "/Users/worawich/Download_dataset/TB/SampleForTestScript/test_separation_01/ERR718193/ERR718193.bam";
+//        String targetBamFile = "/Users/worawich/Download_dataset/TB/SampleForTestScript/test_separation_01/ERR718193/ERR718193_C_sum.fq.gz";
+        String mainBamFile = "/Users/worawich/Download_dataset/TB/SampleForTestScript/debug/reult_with_BWA_cigarSoftclipextract_newunIntersect_nogzip/ERR718193.bam";
+        String targetBamFile = "/Users/worawich/Download_dataset/TB/SampleForTestScript/debug/reult_with_BWA_cigarSoftclipextract_newunIntersect_nogzip/ERR718193_C_map.bam";
         String outputFormat = "sam";
 //        
 //        SVPUtility.addUnmapFlagToBam(mainBamFile, targetBamFile, outputFormat);
